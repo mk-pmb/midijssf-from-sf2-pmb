f14d5750343458c8a6a99902f46fe1b547322838
mk-pmb <mk-pmb@users.noreply.github.com>
2017-09-25 16:45:08 +0200

    v0.1.6: gm1snake

---
+++ js/gm1snake.js
@@ -0,0 +1,6 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+var dfltCfg = require('../cfg.default.js'), ld = require('lodash'),
+  gm1inst = require('midi-instrument-names-gm1-pmb').instruments;
+module.exports = gm1inst.concat(dfltCfg.chn10Name).map(ld.snakeCase);
